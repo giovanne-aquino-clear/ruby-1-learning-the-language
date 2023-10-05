5d135274ad0ea8dc25db08e1053e1647ab5b64e8
Giovanne Aquino <giovanne.aquino.clear@gmail.com>
2023-10-05 12:52:49 -0300

    array

---
+++ array.rb
@@ -0,0 +1,13 @@
+v=[1,3,5,7,9]
+
+v.each do |item|
+    puts item
+end 
+
+puts "posição " + v[2].to_s
+
+nome = "giovanne"
+
+puts "pegar primeira letra do nome: " + nome[0]
+puts nome[0]+nome[1]+nome[2]
+
